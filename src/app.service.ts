c763494fe20824a608acb44e256a021b2a07af90
Omkar Santosh Barbade <omkarbarbade12800@gmail.com>
2026-01-02 10:03:11 +0530

    Resolved Jenkins file issue

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<h1>This is NestJS App Running from EC2 instance<h1><br><br>Hello World!';
   }
 }
