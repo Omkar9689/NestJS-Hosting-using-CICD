c02adf5b3036bf0c667665811a97ba5c5b74345a
Omkar Santosh Barbade <omkarbarbade12800@gmail.com>
2026-01-02 10:04:10 +0530

    Edited Web Page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>This is NestJS App Running from EC2 instance<h1><br><br>Hello World!';
+    return '<h1>This is NestJS App Running from EC2 instance</h1><br><br><h3>Hello World!</h3>';
   }
 }
